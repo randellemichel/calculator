5a13c66d5ea1300ef0d2180fe4c4c61a49e98a6b
randellemichel <randellemichel@gmail.com>
2017-01-23 13:30:12 +0000

    multiplication update

--- Calculator.java
+++ Calculator.java
@@ -22,4 +22,10 @@ public class Calculator{
       z = x + y;
       System.out.println("result: "+ z);
     }
+
+    //multiplication
+    else if(op == 3){
+      z = x * y;
+      System.out.println("result: "+ z);
+    }
 }
